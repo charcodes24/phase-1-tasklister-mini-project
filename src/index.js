4040219c7b4c8ef37abb18ef6bcf8485cf390ddb
Charlene Johnson <charcodes24@gmail.com>
2021-05-05 15:23:10 -0700

    Completed assignment.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.forms[0];
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let info = document.getElementById('new-task-description').value;
+    let list = document.createElement('li');
+    list.innerHTML = info;
+    let button = document.createElement('button');
+    button.innerHTML = 'x';
+    list.append(button);
+    button.addEventListener('onclick', (e) => {
+      this.closest('li').innerHTML.remove();
+    })
+    document.getElementById('tasks').appendChild(list);
+  });
+  
 });
+
+
+
+//get info out of input
+//create new li 
+//info gets into li
+//get li onto the page
+
+//create button 
+//button has x
+//button on page
+//event listener for button
+//function for button that deletes li ** could be broken into smaller tasks
+
